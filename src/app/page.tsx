b2c62d6f2857af723c339b4ecbffc68b4a1b5cb4
LANDIN\junio <juniorlandin10@gmail.com>
2025-08-29 20:40:55 -0300

    Feat: New Layout Login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,14 @@
 'use client';
 
 import authClient from "@/api/auth/client";
+import ImageLogin from "@/components/layout/ImageLogin";
 import { Button } from '@/components/ui/button';
-import { Label } from '@/components/ui/label';
 import { TextField } from '@/components/ui/text-field';
 import { PAGES_DEF } from "@/constants/pages";
 import { cn } from '@/utils/utils';
-import { UserIcon, LockIcon } from '@phosphor-icons/react';
+import { LockIcon, UserIcon } from '@phosphor-icons/react';
 import Image from "next/image";
 import { useRouter } from "next/navigation";
-import path from "path";
 import { FormEvent, useState } from 'react';
 import { toast } from 'sonner';
 
@@ -96,7 +95,7 @@ export default function LoginPage() {
               error={usernameError}
               disabled={isLoading}
               inputClassName={cn(
-                "py-6 px-12 rounded-full border border-[#E9E9E9] placeholder-[#A3A3A3]"
+                "py-6 px-12 h-[58px] rounded-full border border-[#E9E9E9] placeholder-[#A3A3A3]"
               )}
             />
 
@@ -113,14 +112,14 @@ export default function LoginPage() {
               startIcon={LockIcon}
               disabled={isLoading}
               inputClassName={cn(
-                "py-6 px-12 rounded-full border border-[#E9E9E9] placeholder-[#A3A3A3]"
+                "py-6 px-12 h-[58px] rounded-full border border-[#E9E9E9] placeholder-[#A3A3A3]"
               )}
             />
 
             <Button
               label='Entrar'
               type="submit"
-              className="py-6 w-full h-12 text-white text-md font-medium transition-colors bg-[#276EEB]"
+              className="py-6 w-full h-[58px] text-white text-md font-medium transition-colors bg-[#276EEB]"
               disabled={isLoading}
               isLoading={isLoading}
             />
@@ -129,55 +128,7 @@ export default function LoginPage() {
 
       </form>
 
-      <div className="relative w-[55%] h-[90vh] rounded-4xl overflow-hidden [--gap:16px]">
-        {/* Imagem de fundo */}
-        <Image
-          src="/images/rodovia-login.jpg"
-          alt="Rodovia"
-          fill
-          className="object-cover"
-          priority
-        />
-
-        {/* Borda branca externa = metade do gap */}
-        <div className="pointer-events-none absolute inset-0 z-10 rounded-4xl" />
-
-        {/* Overlay GRID (sem gap!) */}
-        {/* Overlay GRID */}
-        <div className="absolute inset-0 z-20 grid gap-2 [grid-template-columns:30%_40%_30%]">
-          {/* COL 1 */}
-          <div className="grid grid-rows-[.66fr_1fr_1fr_.25fr] gap-2 min-w-0">
-            <div
-              className="
-                relative
-                rounded-br-4xl bg-transparent backdrop-brightness-75
-                [box-shadow:0_0_0_calc(var(--gap)/2)_white]
-                [--extra-br:20px]
-              "
-            />
-            <div className="border-r-4xl bg-transparent backdrop-brightness-75 [box-shadow:0_0_0_calc(var(--gap)/2)_white]" />
-            <div className="border-r-4xl bg-transparent backdrop-brightness-75 [box-shadow:0_0_0_calc(var(--gap)/2)_white]" />
-            <div className="border-r-4xl bg-transparent backdrop-brightness-75 [box-shadow:0_0_0_calc(var(--gap)/2)_white]" />
-          </div>
-
-          {/* COL 2 */}
-          <div className="grid grid-rows-3 gap-2 min-w-0">
-            <div className=" bg-transparent backdrop-brightness-75 [box-shadow:0_0_0_calc(var(--gap)/2)_white]" />
-            <div className=" bg-transparent backdrop-brightness-75 [box-shadow:0_0_0_calc(var(--gap)/2)_white]" />
-            <div className=" bg-transparent backdrop-brightness-75 [box-shadow:0_0_0_calc(var(--gap)/2)_white]" />
-          </div>
-
-          {/* COL 3 */}
-          <div className="grid grid-rows-5 gap-2 min-w-0">
-            <div className=" bg-transparent backdrop-brightness-75 [box-shadow:0_0_0_calc(var(--gap)/2)_white]" />
-            <div className=" bg-transparent backdrop-brightness-75 [box-shadow:0_0_0_calc(var(--gap)/2)_white]" />
-            <div className=" bg-transparent backdrop-brightness-75 [box-shadow:0_0_0_calc(var(--gap)/2)_white]" />
-            <div className=" bg-transparent backdrop-brightness-75 [box-shadow:0_0_0_calc(var(--gap)/2)_white]" />
-            <div className=" bg-transparent backdrop-brightness-75 [box-shadow:0_0_0_calc(var(--gap)/2)_white]" />
-          </div>
-        </div>
-
-      </div>
+      <ImageLogin />
     </div>
   );
 }
